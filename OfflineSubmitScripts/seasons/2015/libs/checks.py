c054a978f1d0a8dcd831e59a12e0bea9441d2cb8
Jan Oertlin <jan.oertlin@icecube.wisc.edu>
2016-04-11 14:24:06 +0000

    Merge with trunk. Fixed PostProcessing issue

--- OfflineSubmitScripts/seasons/2015/libs/checks.py
+++ OfflineSubmitScripts/seasons/2015/libs/checks.py
@@ -5,6 +5,7 @@ Functions to perform checks.
 """
 
 import glob
+import re
 import os
 import stat
 import subprocess as sub
@@ -149,7 +150,7 @@ def CheckFiles(r,logger,dryrun=False):
     # make symlink to latest output dir
     baseDir = ICECUBE_DATADIR(r["tStart"].year) + "/%s%s"%\
               (str(r['tStart'].month).zfill(2),str(r['tStart'].day).zfill(2))
-    OutDirs = [g.split("_")[-1] for g in glob.glob(os.path.join(baseDir,"Run00%s_*"%r['run_id']))]
+    OutDirs = [g.split("_")[-1] for g in os.listdir(baseDir) if re.search(r"Run%s_[0-9]+"%str(r['run_id']).zfill(8), g)]
     OutDirs.sort(key=int)
     LatestDir = os.path.join(baseDir,"Run00%s_%s"%(r['run_id'],OutDirs[-1]))
     
