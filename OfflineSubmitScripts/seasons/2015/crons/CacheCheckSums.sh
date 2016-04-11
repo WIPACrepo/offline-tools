3a0cc070b4b25dd98dfeff1b9fbe1b05c65ec375
Jan Oertlin <jan.oertlin@icecube.wisc.edu>
2016-04-11 14:21:52 +0000

    Debug crons

--- OfflineSubmitScripts/seasons/2015/crons/CacheCheckSums.sh
+++ OfflineSubmitScripts/seasons/2015/crons/CacheCheckSums.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 
-$I_ENV python /data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts_2015/CacheChksums_2015.py --debug > /data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts_2015/cronlog.txt 2>&1
+$I_ENV python /data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts_2015/CacheChksums_2015.py --debug > /data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts_2015/logs/PreProcessing/CacheCheckSumsOutput.log 2>&1
--- OfflineSubmitScripts/seasons/2015/crons/TemplateGCDChecks.sh
+++ OfflineSubmitScripts/seasons/2015/crons/TemplateGCDChecks.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-$I_ENV python /data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts_2015/TemplateGCDChecks.py 
+$I_ENV python /data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts_2015/TemplateGCDChecks.py --debug > /data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts_2015/logs/TemplateGCDChecks/output.log 2>&1 
