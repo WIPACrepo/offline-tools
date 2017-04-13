894151fa83318e168fb71d97f76007239ffe8c30
Jan Oertlin <jan.oertlin@icecube.wisc.edu>
2017-04-13 16:41:59 +0000

    Modified dbs4 -> filter-db copy script: Postprocessing data is also copied and duplicates will be updated

--- Snippets/Dbs4ToFilterDb.py
+++ Snippets/Dbs4ToFilterDb.py
@@ -5,91 +5,153 @@ sys.path.append('/data/user/i3filter/IC86_OfflineProcessing/OfflineSubmitScripts
 from libs.databaseconnection import DatabaseConnection
 from libs.logger import DummyLogger
 
+def grl_to_runs(dbs4, filter_db):
+    dbs4_sql = 'SELECT * FROM i3filter.grl_snapshot_info g JOIN i3filter.run_info_summary s ON g.run_id = s.run_id ORDER BY g.run_id, snapshot_id'
+    
+    filter_db_sql = '''INSERT INTO `i3filter`.`runs`
+    (`run_id`,
+    `snapshot_id`,
+    `production_version`,
+    `good_i3`,
+    `good_it`,
+    `reason_i3`,
+    `reason_it`,
+    `good_tstart`,
+    `good_tstart_frac`,
+    `good_tstop`,
+    `good_tstop_frac`,
+    `tstart`,
+    `tstart_frac`,
+    `tstop`,
+    `tstop_frac`,
+    `nevents`,
+    `rate`,
+    `gcd_generated`,
+    `gcd_bad_doms_validated`,
+    `gcd_pole_validation`,
+    `gcd_template_validation`,
+    `active_strings`,
+    `active_doms`,
+    `active_in_ice_doms`)
+    VALUES
+    ({run_id},
+    {snapshot_id},
+    {production_version},
+    {good_i3},
+    {good_it},
+    '{reason_i3}',
+    '{reason_it}',
+    '{good_tstart}',
+    {good_tstart_frac},
+    '{good_tstop}',
+    {good_tstop_frac},
+    '{tstart}',
+    {tstart_frac},
+    '{tstop}',
+    {tstop_frac},
+    {nevents},
+    {rate},
+    {gcd_generated},
+    {gcd_bad_doms_validated},
+    {gcd_pole_validation},
+    {gcd_template_validation},
+    {active_strings},
+    {active_doms},
+    {active_in_ice_doms})
+    ON DUPLICATE KEY UPDATE
+        good_i3 = {good_i3},
+        good_it = {good_it},
+        reason_i3 = '{reason_i3}',
+        reason_it = '{reason_it}',
+        good_tstart = '{good_tstart}',
+        good_tstart_frac = {good_tstart_frac},
+        good_tstop = '{good_tstop}',
+        good_tstop_frac = {good_tstop_frac},
+        tstart = '{tstart}',
+        tstart_frac = {tstart_frac},
+        tstop = '{tstop}',
+        tstop_frac = {tstop_frac},
+        nevents = {nevents},
+        rate = {rate},
+        gcd_generated = {gcd_generated},
+        gcd_bad_doms_validated = {gcd_bad_doms_validated},
+        gcd_pole_validation = {gcd_pole_validation},
+        gcd_template_validation = {gcd_template_validation},
+        active_strings = {active_strings},
+        active_doms = {active_doms},
+        active_in_ice_doms = {active_in_ice_doms}
+    '''
+    
+    s = dbs4.fetchall(dbs4_sql, UseDict = True)
+    
+    counter = 0
+    for e in s:
+        for k in e.keys():
+            if e[k] is None:
+                e[k] = 'NULL'
+    
+        counter += 1
+    
+        sql = filter_db_sql.format(
+            tstart = e['tStart'],
+            tstart_frac = e['tStart_frac'],
+            tstop = e['tStop'],
+            tstop_frac = e['tStop_frac'],
+            nevents = e['nEvents'],
+            rate = e['rateHz'],
+            active_strings = e['ActiveStrings'],
+            active_doms = e['ActiveDOMs'],
+            active_in_ice_doms = e['ActiveInIceDOMs'],
+            gcd_generated = e['GCDCheck'],
+            gcd_bad_doms_validated = e['BadDOMsCheck'],
+            gcd_pole_validation = e['PoleGCDCheck'],
+            gcd_template_validation = e['TemplateGCDCheck'],
+            **e
+        )
+ 
+#        print sql
+   
+        print "[{0:>4} / {1}] run_id = {2}".format(counter, len(s), e['run_id'])
 
-dbs4_sql = 'SELECT * FROM i3filter.grl_snapshot_info g JOIN i3filter.run_info_summary s ON g.run_id = s.run_id ORDER BY g.run_id, snapshot_id'
+        filter_db.execute(sql)
 
-filter_db_sql = '''INSERT INTO `i3filter`.`runs`
-(`run_id`,
-`snapshot_id`,
-`production_version`,
-`good_i3`,
-`good_it`,
-`reason_i3`,
-`reason_it`,
-`good_tstart`,
-`good_tstart_frac`,
-`good_tstop`,
-`good_tstop_frac`,
-`tstart`,
-`tstart_frac`,
-`tstop`,
-`tstop_frac`,
-`nevents`,
-`rate`,
-`gcd_generated`,
-`gcd_bad_doms_validated`,
-`gcd_pole_validation`,
-`gcd_template_validation`,
-`active_strings`,
-`active_doms`,
-`active_in_ice_doms`)
-VALUES
-({run_id},
-{snapshot_id},
-{production_version},
-{good_i3},
-{good_it},
-'{reason_i3}',
-'{reason_it}',
-'{good_tstart}',
-{good_tstart_frac},
-'{good_tstop}',
-{good_tstop_frac},
-'{tstart}',
-{tstart_frac},
-'{tstop}',
-{tstop_frac},
-{nevents},
-{rate},
-{gcd_generated},
-{gcd_bad_doms_validated},
-{gcd_pole_validation},
-{gcd_template_validation},
-{active_strings},
-{active_doms},
-{active_in_ice_doms})
-'''
+def post_processing(dbs4, filter_db):
+    s = dbs4.fetchall('SELECT * FROM i3filter.offline_postprocessing', UseDict = True)
+
+    filter_db_sql = '''INSERT INTO `i3filter`.`post_processing`
+        (`run_id`,
+        `dataset_id`,
+        `validated`,
+        `date_of_validation`)
+        VALUES
+        ({run_id},
+        {dataset_id},
+        {validated},
+        '{date_of_validation}')
+    ON DUPLICATE KEY UPDATE
+        validated = {validated},
+        date_of_validation = '{date_of_validation}'
+    '''
+
+    counter = 0
+    for e in s:
+        for k in e.keys():
+            if e[k] is None:
+                e[k] = 'NULL'
+    
+        counter += 1
+
+        sql = filter_db_sql.format(**e)
+
+        #print sql
+
+        print "[{0:>4} / {1}] run_id = {2}, dataset_id = {3}".format(counter, len(s), e['run_id'], e['dataset_id'])
+
+        filter_db.execute(sql)
 
 dbs4 = DatabaseConnection.get_connection('dbs4', DummyLogger())
 filter_db = DatabaseConnection.get_connection('filter-db', DummyLogger())
 
-s = dbs4.fetchall(dbs4_sql, UseDict = True)
+grl_to_runs(dbs4 = dbs4, filter_db = filter_db)
+post_processing(dbs4 = dbs4, filter_db = filter_db)
 
-counter = 0
-for e in s:
-    for k in e.keys():
-        if e[k] is None:
-            e[k] = 'NULL'
-
-    counter += 1
-
-    sql = filter_db_sql.format(
-        tstart = e['tStart'],
-        tstart_frac = e['tStart_frac'],
-        tstop = e['tStop'],
-        tstop_frac = e['tStop_frac'],
-        nevents = e['nEvents'],
-        rate = e['rateHz'],
-        active_strings = e['ActiveStrings'],
-        active_doms = e['ActiveDOMs'],
-        active_in_ice_doms = e['ActiveInIceDOMs'],
-        gcd_generated = e['GCDCheck'],
-        gcd_bad_doms_validated = e['BadDOMsCheck'],
-        gcd_pole_validation = e['PoleGCDCheck'],
-        gcd_template_validation = e['TemplateGCDCheck'],
-        **e
-    )
-
-    print "[{0:>4} / {1}] run_id = {2}".format(counter, len(s), e['run_id'])
-
-    filter_db.execute(sql)
