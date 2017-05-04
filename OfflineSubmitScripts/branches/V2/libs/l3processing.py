6d395d92edf4a3a849bb36c96da9bef8f93b285b
Jan Oertlin <jan.oertlin@icecube.wisc.edu>
2017-05-04 19:14:46 +0000

    Ooops, forgot a file

---
+++ OfflineSubmitScripts/branches/V2/libs/l3processing.py
@@ -0,0 +1,26 @@
+
+frim files import File
+
+def get_gcd_file(run, args, config, logger):
+    if not args.cosmicray:
+        # Just use the normal GCD file
+        return run.get_gcd_file()
+    else:
+        # The cosmic ray WG uses special GCD files
+        cr_gcd_file = File(run.format(config.get('Level3', 'CosmicRayGCD')))
+
+        if not cr_gcd_file.exists():
+            logger.error('Did not find CosmicRay GCD file: {0}'.format(cr_gcd_file))
+            return None
+        else:
+            return cr_gcd_file
+
+def get_cosmicray_mc_gcd_file(season, config):
+    mc_gcd_files = config.get_var_dict('Level3', 'CosmicRayMCGCD_', keytype = int)
+
+    if season not in mc_gcd_files:
+        return None
+    else:
+        return File(mc_gcd_files[season])
+
+
