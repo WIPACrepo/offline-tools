2b2259eed38f74ed6ce98106fa29db1590b05270
Jan Oertlin <jan.oertlin@icecube.wisc.edu>
2017-06-07 14:19:26 +0000

    Add SuperDST processing support

---
+++ OfflineSubmitScripts/seasons/2016/MainSubmit_SDST.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+
+"""
+Creates PFDST entries in 'url_path', 'jobs', and 'run' tables in dbs4
+to be used by IceProd in creating/submitting SDST jobs.
+"""
+
+import sys, os
+from os.path import expandvars, join, exists
+import glob
+import time
+import datetime
+from dateutil.relativedelta import *
+
+from RunTools import *
+from FileTools import *
+from libs.logger import get_logger
+from libs.argparser import get_defaultparser
+from libs.files import get_logdir, get_tmpdir, get_existing_check_sums, write_meta_xml_main_processing
+from libs.checks import runs_already_submitted
+from libs.runs import get_run_status, clean_run
+from libs.dbtools import max_queue_id 
+from libs.config import get_dataset_id_by_run
+from libs.databaseconnection import DatabaseConnection
+from libs.utils import DBChecksumCache
+
+##-----------------------------------------------------------------
+## setup DB
+##-----------------------------------------------------------------
+
+def get_submitted_runs(dataset_id, db, logger):
+    submitted_runs = []
+
+    sql = "SELECT DISTINCT run_id FROM i3filter.run WHERE dataset_id = %s" % dataset_id
+    result = db.fetchall(sql, UseDict = True)
+
+    for row in result:
+        submitted_runs.append(int(row['run_id']))
+
+    return list(set(submitted_runs))
+
+def is_good_run(run_id, db, logger):
+    sql = "SELECT run_id, good_it, good_i3 FROM i3filter.grl_snapshot_info WHERE run_id = %s ORDER BY snapshot_id DESC LIMIT 1" % run_id
+
+    result = db.fetchall(sql, UseDict = True)
+
+    logger.debug(result)
+
+    if len(result):
+        return int(result[0]['good_it']) and int(result[0]['good_i3'])
+    else:
+        return False
+
+def submit_run(checksumcache, db, run_id, status, DatasetId, QueueId, dryrun, logger):
+    # Using grid or NPX?
+    grid = db.fetchall("SELECT * FROM i3filter.grid_statistics WHERE dataset_id = %s;" % DatasetId, UseDict = True)
+
+    logger.debug("DB result = %s" % grid)
+
+    path_prefix = 'gsiftp://gridftp.icecube.wisc.edu'
+
+    InFiles = []
+   
+    run_info = db.fetchall("SELECT * FROM i3filter.grl_snapshot_info g JOIN i3filter.run_info_summary s ON g.run_id = s.run_id WHERE g.run_id = %s ORDER BY snapshot_id DESC LIMIT 1" % run_id, UseDict = True)
+ 
+    logger.debug("run_info = %s" % run_info)
+
+    g = run_info[0]
+
+    sDay = g['tStart']      # run start date
+    sY = sDay.year
+    sM = str(sDay.month).zfill(2)
+    sD = str(sDay.day).zfill(2)
+    
+    logger.debug('Get PFFilt files')
+
+    InFiles = glob.glob("/data/exp/IceCube/%s/unbiased/PFRaw/%s%s/*PFRaw_PhysicsFiltering_Run00%s_Subrun00000000_00000*.tar.gz" % (sY, sM, sD, run_id))
+
+    nextDate = sDay + relativedelta(days = 1)
+
+    InFiles.extend(glob.glob("/data/exp/IceCube/%s/unbiased/PFRaw/%s%s/*PFRaw_PhysicsFiltering_Run00%s_Subrun00000000_00000*.tar.gz" % (nextDate.year, str(nextDate.month).zfill(2), str(nextDate.day).zfill(2), run_id)))       
+   
+    InFiles.sort()
+
+    logger.debug("InFiles = %s" % InFiles)
+
+    logger.info("Found %s PFRaw files for this run" % len(InFiles))
+ 
+    MainOutputDir = OutputDir = "/data/user/joertlin/PFDST/%s/%s%s/"%(sY,sM,sD)
+    if not os.path.exists(MainOutputDir) and not dryrun:
+        os.makedirs(MainOutputDir)
+    
+    if not os.path.exists(OutputDir) and not dryrun:
+        os.makedirs(OutputDir)
+    
+    logger.debug('Find GCD file')
+
+    GCDFileName = []
+    GCDFileName = glob.glob("/data/exp/IceCube/%s/filtered/level2/VerifiedGCD/*Run00%s*%s_%s*"%(sY,g['run_id'],str(g['production_version']),str(g['snapshot_id'])))
+
+    if not len(GCDFileName):
+        GCDFileName = glob.glob("/data/exp/IceCube/%s/filtered/level2/AllGCD/*Run00%s*%s_%s*"%(sY,g['run_id'],str(g['production_version']),str(g['snapshot_id'])))
+    
+    logger.debug("GCD files = %s" % GCDFileName)
+    
+    if len(GCDFileName):
+        logger.debug('Calculate MD5 sum and create symlink for GCD file')
+
+        GCDFileName = GCDFileName[0]
+        GCDFileChkSum = str(FileTools(GCDFileName, logger).md5sum())
+        
+        lnGCDFile = os.path.join(OutputDir,os.path.basename(GCDFileName))
+       
+        logger.debug("ln -sf %s %s"%(GCDFileName, lnGCDFile))
+ 
+        if not dryrun:
+            lnCmd = "ln -sf %s %s"%(GCDFileName, lnGCDFile)
+            os.system(lnCmd)
+    else:
+        GCDFileName = ""
+        logger.critical("No GCD file found.")
+    
+    if not len(InFiles):
+        logger.info("No PFRaw will be submitted for run %s"%g['run_id'])
+    
+        QueueId+=1
+    
+        if not dryrun:
+            db.execute("""insert into i3filter.job (dataset_id,queue_id,status) values (%s,%s,"%s")"""%(DatasetId,QueueId,status))
+            db.execute("""insert into i3filter.run (run_id,dataset_id,queue_id,sub_run,date) values (%s,%s,%s,%s,"%s")"""%(g['run_id'],DatasetId,QueueId,-1,str(sDay.date())))
+    else:
+        logger.info("Attempting to submit %s PFRaw Files for run %s"%(str(len(InFiles)),g['run_id']))
+    
+        for InFile in InFiles:
+            CountSubRun = int(InFile[-15:-7])
+            
+            logger.info("Submission of file %s / %s -> %s" % (CountSubRun, len(InFiles), InFile))
+
+            QueueId+=1
+    
+            if not dryrun:
+                db.execute("""insert into i3filter.job (dataset_id,queue_id,status) values (%s,%s,"%s")"""%(DatasetId,QueueId,status))
+    
+            if os.path.isfile(GCDFileName) and not dryrun:
+                db.execute("""insert into i3filter.urlpath (dataset_id,queue_id,name,path,type,md5sum,size) values ("%s","%s","%s","%s","INPUT","%s","%s")"""% \
+                             (DatasetId,QueueId,os.path.basename(GCDFileName),path_prefix+os.path.dirname(GCDFileName)+"/",GCDFileChkSum,str(os.path.getsize(GCDFileName))))
+    
+            InFileChkSum = checksumcache.get_md5(InFile)
+    
+            if not dryrun:
+                db.execute("""insert into i3filter.urlpath (dataset_id,queue_id,name,path,type,md5sum,size) values ("%s","%s","%s","%s","INPUT","%s","%s")"""% \
+                             (DatasetId,QueueId,os.path.basename(InFile),path_prefix+os.path.dirname(InFile)+"/",InFileChkSum,str(os.path.getsize(InFile))))
+    
+                db.execute("""insert into i3filter.run (run_id,dataset_id,queue_id,sub_run,date) values (%s,%s,%s,%s,"%s")"""%(g['run_id'],DatasetId,QueueId,CountSubRun,str(sDay.date())))
+
+def main(args, logger):
+    db = DatabaseConnection.get_connection('dbs4', logger)
+
+    submitted_runs = get_submitted_runs(args.datasetid, db, logger)
+    checksumcache = DBChecksumCache(logger, args.dryrun)
+
+    for run_id in args.run:
+        logger.info("********* Run %s *********" % run_id)
+
+        if not is_good_run(run_id, db, logger):
+            logger.error("Run %s is not a good run. Run will be skipped." % run_id)
+            continue
+
+        if run_id in submitted_runs:
+            if args.resubmission:
+                logger.info("Run %s will be resubmitted" % run_id)
+            else:
+                logger.info("Run %s will be skipped" % run_id)
+                continue
+        else:
+            logger.info("Run %s will be submitted" % run_id)
+
+        
+        clean_run(db, args.datasetid, run_id, args.cleandatawarehouse, None, logger, args.dryrun, ignore_production_version = True)
+        
+        qId = max_queue_id(db, args.datasetid)
+            
+        submit_run(checksumcache, db, run_id, 'WAITING', args.datasetid, qId, args.dryrun, logger)
+
+if __name__ == '__main__':
+    parser = get_defaultparser(__doc__, dryrun = True)
+
+    parser.add_argument("--datasetid", type = int, required = True, help = "The dataset id")
+
+
+    parser.add_argument("--run", nargs = '+', type = int, required = True, help = "Run number(s) to process")
+
+    parser.add_argument("--cleandatawarehouse", action = "store_true", default = False, help = "Clean output files in datawarehouse as part of (re)submission process.")
+
+    parser.add_argument("--resubmission", action = "store_true", default = False,
+              help = "If a run has already been submitted, resubmit it. If this option is not set, alrady submitted runs are skipped.")
+
+    args = parser.parse_args()
+
+    logfile = os.path.join(get_logdir(), 'SDST_%s_' % args.datasetid)
+
+    logger = get_logger(args.loglevel, logfile)
+
+    main(args, logger)
---
+++ OfflineSubmitScripts/seasons/2016/PostProcessing_SDST.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python
+"""
+Combines several checks wich have to be done after the
+files are generated and updates the databases accordingly
+"""
+
+
+import os, sys
+import subprocess as sub
+import time
+import datetime
+import argparse
+from dateutil.relativedelta import *
+
+import libs.files
+from libs.config import get_seasons_info, get_config, get_dataset_id_by_run, get_season_by_run
+sys.path.append(get_config().get('DEFAULT', 'SQLClientPath'))
+sys.path.append(get_config().get('DEFAULT', 'ProductionToolsPath'))
+from RunTools import RunTools
+from FileTools import *
+from DbTools import *
+
+from libs.files import get_tmpdir, get_logdir, tar_log_files, insert_gap_file_info_and_delete_files
+from libs.logger import get_logger
+from libs.argparser import get_defaultparser
+
+from libs.checks import PrintVerboseDifference
+
+import libs.process
+
+from libs.databaseconnection import DatabaseConnection
+from libs import dbtools
+
+import SQLClient_i3live as live
+import SQLClient_dbs4 as dbs4
+import SQLClient_dbs2 as dbs2
+
+m_live = live.MySQL()    
+dbs4_ = dbs4.MySQL()   
+dbs2_ = dbs2.MySQL()    
+
+def remove_path_prefix(path):
+    """
+    Removes `file:` or `gsiftp://gridftp.icecube.wisc.edu` from path.
+    """
+
+    prefix = ['file:', 'gsiftp://gridftp.icecube.wisc.edu']
+
+    for p in prefix:
+        if path.startswith(p):
+            return path[len(p):]
+
+    return path
+
+def get_in_files(run_id, start_date):
+    return get_x_files(run_id, start_date, '/data/exp/IceCube/{year}/unbiased/PFRaw/{month:0>2}{day:0>2}/*PFRaw_PhysicsFiltering_Run{run_id:0>8}_Subrun00000000_00000*.tar.gz')
+
+def get_out_files(run_id, start_date, season):
+    dst_folder = 'PFDST'
+    if int(season) in (2015, 2016):
+        dst_folder += 'noSPE'
+
+    return get_x_files(run_id, start_date, '/data/exp/IceCube/{year}/unbiased/' + dst_folder + '/{month:0>2}{day:0>2}/PFDST_PhysicsFiltering_Run*{run_id:0>8}_Subrun00000000_00000*.i3.bz2')
+
+def get_x_files(run_id, start_date, folder_pattern):
+    from glob import glob
+
+    def f(d, date, run_id):
+        return d.format(year = date.year, month = date.month, day = date.day, run_id = run_id)
+
+    next_day = start_date + relativedelta(days = 1)
+
+    folder1 = f(folder_pattern, start_date, run_id)
+    folder2 = f(folder_pattern, next_day, run_id)
+
+    gcd = f(os.path.join(os.path.dirname(folder_pattern), '*{run_id}*GCD*'), start_date, run_id)
+
+    files = glob(folder1)
+    files.extend(glob(folder2))
+    files.extend(glob(gcd))
+
+    return files
+
+ICECUBE_GCDDIR = lambda x : "/data/exp/IceCube/%s/filtered/level2/VerifiedGCD" %str(x)
+def CheckFiles(r, logger, dataset_id, season, run_id, dryrun):
+    if not r['GCDCheck'] or not r['BadDOMsCheck']:
+        logger.info("GCDCheck or BadDOMsCheck failed for run=%s, production_version%s" %(str(r['run_id']),str(r['production_version'])))
+        return 1
+    
+    InFiles = get_in_files(run_id, r['tStart'])
+    OutFiles = get_out_files(run_id, r['tStart'], season)
+  
+    #logger.debug('OutFiles: %s' % OutFiles)
+ 
+    # Remove all BadRuns from InFiles:
+    bad_runs = dbtools.get_bad_sub_runs(dbs4 = dbs4_, dataset_id = dataset_id, run_id = r['run_id'], logger = logger)
+
+    for bad_run in bad_runs:
+        path = os.path.join(bad_run['path'], bad_run['name'])[5:]
+        InFiles = filter(lambda e: e != path, InFiles)
+
+    ProdVersion = "%s_%s/"%(str(r['run_id']),str(r['production_version']))
+    
+    Files2Check = []
+    
+    # check for multiple GCD files in out Dir, usually results from re-processing
+    
+    GCDName = [f for f in OutFiles if "GCD" in f]
+
+    if len(GCDName)!=1:
+        logger.warning("Either None or more than 1 GCD file in output dir for run=%s"%str(r['run_id']))
+        return 1
+    
+    GCDName = GCDName[0]
+    GCDName = os.path.join(ICECUBE_GCDDIR(r['tStart'].year),os.path.basename(GCDName))
+    Files2Check.append(GCDName)    
+    
+    L2Files = [f for f in OutFiles if "GCD" not in f \
+                   and "txt" not in f and "root" not in f\
+                   and "EHE" not in f and "IT" not in f \
+                   and "log" not in f]  
+    L2Files.sort()
+    
+    logger.debug('InFiles: %s' % InFiles)
+    logger.debug('OutFiles: %s' % L2Files)
+
+    if len(InFiles) != len(L2Files):
+        logger.warning("No. of Input and Output files don't match for run=%s, production_version=%s" %(str(r['run_id']),str(r['production_version'])))
+        return 1
+
+    for p in InFiles:
+        l = os.path.join(
+            os.path.dirname(L2Files[0]),
+            'PFDST_' + os.path.basename(p).split('PFRaw_')[1].replace(".tar",".i3").replace('.gz', '.bz2')
+        )
+    
+        if not os.path.isfile(l):
+            logger.warning("At least one output file %s does not exist for input file %s"%(l,p))
+            return 1
+
+        Files2Check.append(p)
+        Files2Check.append(l)
+    
+    Files2CheckS = """'""" + """','""".join(Files2Check) + """'"""
+    
+    sql = """SELECT distinct name, path
+                                  FROM i3filter.urlpath u
+                                 WHERE u.dataset_id = {dataset_id} AND (
+                                 concat(substring(u.path,6),"/",u.name) IN ({files}) OR \
+                                 concat(substring(u.path,6),u.name) IN ({files}) OR \
+                                 concat(substring(u.path,34),u.name) IN ({files}) OR \
+                                 concat(substring(u.path,34),"/",u.name) IN ({files}))""".format(dataset_id = dataset_id, files = Files2CheckS)
+ 
+    logger.debug('SQL: {0}'.format(sql))
+ 
+    FilesInDb = dbs4_.fetchall(sql, UseDict = True)
+ 
+    logger.debug(FilesInDb)
+ 
+    FilesInDb = list(set([os.path.join(remove_path_prefix(f['path']), f['name']) for f in FilesInDb]))
+
+    logger.debug('Files = %s' % FilesInDb)
+
+    if len(Files2Check) != len(FilesInDb):
+        logger.warning("Some file records don't exist for run=%s, production_version=%s" %(str(r['run_id']),str(r['production_version'])))
+        PrintVerboseDifference(Files2Check,FilesInDb,logger) 
+        return 1
+   
+    # Check MD5 sums
+    outputFileInfos = dbs4_.fetchall("""SELECT sub_run, name, path, size, md5sum
+                                        FROM run r
+                                        JOIN urlpath u 
+                                            ON u.queue_id = r.queue_id 
+                                            AND u.dataset_id = r.dataset_id 
+                                        WHERE   r.dataset_id = %s 
+                                            AND type = 'PERMANENT'
+                                            AND transferstate <> 'DELETED' 
+                                            AND run_id = %s""" % (dataset_id, r['run_id']),
+                                    UseDict = True)
+
+    for i, subrunInfo in enumerate(outputFileInfos):
+        path = os.path.join(subrunInfo['path'][5:], subrunInfo['name'])
+
+        if path in OutFiles:
+            logger.info('(%s / %s)\tCheck checksums for %s' % (i, len(outputFileInfos), path))
+
+            md5sum = FileTools(FileName = path, logger = logger).md5sum()
+
+            # Check if checksum matches the checksum in DB
+            if md5sum == subrunInfo['md5sum']:
+                logger.debug("MD5 check sums match for %s" % path)
+            else:
+                logger.warning("MD5 check sum mismatch for %s" % path)
+                return 1
+        else:
+            logger.warning("File %s is listed in the database as PERMANENT but doesn not exist" % path)
+            return 1
+ 
+    # all checks passed
+    return 0
+
+def main_run(r, logger, season, args):
+    dryrun = args.dryrun
+    dataset_id = args.dataset_id
+
+    logger.info("======= Checking %s %s ==========="  %(str(r['run_id']),str(r['production_version'])))
+
+    if DbTools(r['run_id'], dataset_id).AllOk():
+        logger.warning( """Processing of Run=%s, production_version=%s
+                 may not be complete ... skipping"""\
+                %(r['run_id'],str(r['production_version'])))
+        return    
+     
+    # check i/o files in data warehouse and Db
+    logger.info("Checking Files in Data warehouse and database records ...")
+    if CheckFiles(r, logger, dataset_id = dataset_id, season = season, run_id = r['run_id'], dryrun = dryrun):
+        logger.error("FilesCheck failed: for Run=%s, production_version=%s"\
+        %(r['run_id'],str(r['production_version'])))
+        return
+    logger.info("File checks  .... passed")
+
+    sql = """   INSERT INTO post_processing
+                    (run_id, dataset_id, validated, date_of_validation)
+                VALUES
+                    (%s, %s, %s, NOW())
+                ON DUPLICATE KEY UPDATE
+                    validated = %s,
+                    date_of_validation = NOW()
+                    """ % (r['run_id'], args.dataset_id, 1, 1)
+
+    logger.debug("SQL: %s" % sql)
+
+    if not dryrun:
+        filter_db = DatabaseConnection.get_connection('filter-db', logger)
+        filter_db.execute(sql)
+
+    sDay = r['tStart']
+    sY = sDay.year
+    sM = str(sDay.month).zfill(2)
+    sD = str(sDay.day).zfill(2)
+
+    dst_folder = 'PFDST'
+    if int(season) in (2015, 2016):
+        dst_folder += 'noSPE'
+
+    run_folder = "/data/exp/IceCube/%s/unbiased/%s/%s%s" % (sY, dst_folder, sM, sD)
+
+    logger.debug('tar log files')
+
+    tar_log_files(run_path = run_folder, dryrun = dryrun, logger = logger)
+
+    logger.info("Checks passed")
+    logger.info("======= End Checking %i %i ======== " %(r['run_id'],r['production_version'])) 
+    return
+
+def main(runinfo, logger, season, args):
+    fdb = DatabaseConnection.get_connection('filter-db', logger)
+    sql = "SELECT * FROM i3filter.post_processing WHERE dataset_id = {0} AND validated".format(args.dataset_id)
+    validated_runs = fdb.fetchall(sql, UseDict = True)
+    validated_runs = [int(r['run_id']) for r in validated_runs]
+
+    logger.debug('Validated runs: %s' % validated_runs)
+
+    for run in runinfo:
+        try:
+            if int(run['run_id']) not in validated_runs:
+                main_run(run, logger, season, args)
+            else:
+                logger.info('Skip run %s because it has already been validated' % run['run_id'])
+        except Exception as e:
+            logger.exception("Exception %s thrown for: Run=%s, production_version=%s" %(e.__repr__(),run['run_id'],str(run['production_version'])))
+   
+if __name__ == '__main__':
+    parser = get_defaultparser(__doc__,dryrun=True)
+    parser.add_argument('-r',nargs="?", help="run to postprocess",dest="run",type=int)
+    parser.add_argument('--season', nargs="?", default = None, help="Validate a specific season. If not specified (and not -r) the season that has been set in the confog file will be chosen", type=int)
+    parser.add_argument('--dataset-id', required = True, help="The dataset id", type=int)
+    args = parser.parse_args()
+
+    LOGFILE=os.path.join(get_logdir(), 'SDST_PostProcessing_')
+
+    logger = get_logger(args.loglevel, LOGFILE)
+
+    # Check if --cron option is enabled. If so, check if cron usage allowed by config
+    lock = None
+
+    season = args.season or get_config().getint('DEFAULT', 'Season')
+    seasons_info = get_seasons_info()
+
+    logger.debug('Seaosns info: %s' % seasons_info)
+
+    next_test_runs = []
+    last_run = 9999999
+    if season + 1 in seasons_info:
+        if seasons_info[season + 1]['first'] > 0:
+            last_run = seasons_info[season + 1]['first']
+
+        next_test_runs = seasons_info[season + 1]['test']
+
+    logger.debug('Season: %s' % season)
+
+    RunInfo = None
+
+    if args.run is not None:
+        RunInfo = dbs4_.fetchall("""SELECT r.tStart,g.* FROM i3filter.grl_snapshot_info g
+                                  JOIN i3filter.run_info_summary r ON r.run_id=g.run_id
+                                  WHERE (g.good_i3 OR g.good_it or g.run_id IN (%s)) AND g.run_id = %i
+                                  ORDER BY g.run_id""" % (','.join([str(r) for r in seasons_info[season]['test']]), args.run), UseDict=True)
+    else: 
+        sql = """
+SELECT 
+    *
+FROM
+    i3filter.grl_snapshot_info g
+        JOIN
+    i3filter.run r ON r.run_id = g.run_id
+        AND r.dataset_id = {dataset_id}
+        JOIN
+    i3filter.run_info_summary i ON i.run_id = g.run_id
+WHERE
+    (g.run_id BETWEEN {first_run} AND {last_run}
+        OR g.run_id IN ({test_runs},-1))
+        AND g.run_id NOT IN ({next_test_runs},-1)
+        AND (good_it OR good_i3
+        OR g.run_id IN ({test_runs},-1))
+GROUP BY g.run_id
+ORDER BY g.run_id
+""".format( first_run = seasons_info[season]['first'],
+            test_runs = ','.join([str(r) for r in seasons_info[season]['test']]),
+            last_run = last_run,
+            next_test_runs = ','.join([str(r) for r in next_test_runs]),
+            dataset_id = args.dataset_id
+)
+
+        logger.debug('SQL: {0}'.format(sql))
+
+        RunInfo = dbs4_.fetchall(sql, UseDict=True)
+
+#    logger.debug("RunInfo = %s" % str(RunInfo))
+
+    main(RunInfo, logger, season, args)
+
+    logger.info('Post processing completed')
+
