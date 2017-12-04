268d5f6bfcc1e0cf01bd4f5af3ca01c13e38e24a
Jan Oertlin <jan.oertlin@icecube.wisc.edu>
2017-12-04 14:29:28 +0000

    Add scripts to complete pass2

---
+++ OfflineSubmitScripts/branches/pass2/L3_post_processing.sh
@@ -0,0 +1,20 @@
+
+# Muon
+python PostProcessing_L3.py --sourcedatasetid 1921 --destinationdatasetid 1946 -s 127951 -e 128753
+python PostProcessing_L3.py --sourcedatasetid 1920 --destinationdatasetid 1945 -s 126289 -e 127949
+python PostProcessing_L3.py --sourcedatasetid 1919 --destinationdatasetid 1944 -s 124550 -e 126377
+python PostProcessing_L3.py --sourcedatasetid 1918 --destinationdatasetid 1943 -s 122205 -e 124699
+python PostProcessing_L3.py --sourcedatasetid 1917 --destinationdatasetid 1942 -s 120028 -e 122275
+python PostProcessing_L3.py --sourcedatasetid 1916 --destinationdatasetid 1941 -s 118175 -e 120155
+python PostProcessing_L3.py --sourcedatasetid 1922 --destinationdatasetid 1940 -s 115986 -e 118173
+
+# Cascade
+python PostProcessing_L3.py --sourcedatasetid 1921 --destinationdatasetid 1936 -s 127951 -e 128753
+python PostProcessing_L3.py --sourcedatasetid 1920 --destinationdatasetid 1935 -s 126289 -e 127949
+python PostProcessing_L3.py --sourcedatasetid 1919 --destinationdatasetid 1934 -s 124550 -e 126377
+python PostProcessing_L3.py --sourcedatasetid 1918 --destinationdatasetid 1933 -s 122205 -e 124699
+python PostProcessing_L3.py --sourcedatasetid 1917 --destinationdatasetid 1932 -s 120028 -e 122275
+python PostProcessing_L3.py --sourcedatasetid 1916 --destinationdatasetid 1931 -s 118175 -e 120155
+python PostProcessing_L3.py --sourcedatasetid 1922 --destinationdatasetid 1937 -s 115986 -e 118173
+
+echo "Done"
