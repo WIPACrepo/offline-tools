e4053c5f6fabca421679d89c39b5ca723f75c6da
R Snihur <gh999ic@gmail.com>
2022-07-17 23:54:07 +0000

    Season 2022

---
+++ OfflineSubmitScripts/seasons/2022_ip1/bin/Python.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+eval `/cvmfs/icecube.opensciencegrid.org/py2-v3.1.1/setup.sh`
+python $@
---
+++ OfflineSubmitScripts/seasons/2022_ip1/tests/test_libs_stringmanipulation.py
@@ -0,0 +1,24 @@
+
+import os
+import unittest
+from libs import stringmanipulation
+from libs.logger import DummyLogger
+
+class TestLibsStringmanipulation(unittest.TestCase):
+    def setUp(self):
+        self.logger = DummyLogger()
+
+        self.data_folder = os.path.dirname(os.path.abspath(__file__))
+
+    def test_replace_var(self):
+        test_str = 'Hi {user}, this is a test string with {len:d} chars. This var {v!r:>30} will be replaced with *. This one wont: {t!a:0>4}'
+        test_str_result = 'Hi {user}, this is a test string with {len:d} chars. This var * will be replaced with *. This one wont: {t!a:0>4}'
+
+        self.assertEqual(stringmanipulation.replace_var(test_str, 'v', '*'), test_str_result)
+
+    def test_replace_all_var(self):
+        test_str = 'Hi {user}, this is a test string with {len:d} chars. This var {v!r:>30} will be replaced with *. This one wont: {t!a:0>4}'
+        test_str_result = 'Hi *, this is a test string with * chars. This var * will be replaced with *. This one wont: *'
+
+        self.assertEqual(stringmanipulation.replace_all_vars(test_str, '*'), test_str_result)
+
