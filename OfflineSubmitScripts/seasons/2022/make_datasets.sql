8501df43af29ee0e2f77530ceca208c03a433048
R Snihur <gh999ic@gmail.com>
2022-07-23 17:43:02 +0000

    Update season

--- OfflineSubmitScripts/seasons/2022/make_datasets.sql
+++ OfflineSubmitScripts/seasons/2022/make_datasets.sql
@@ -1,9 +1,9 @@
-insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@L2, "IC86 2021 Offline L2 Data Production", 2021, "L2", 1, 0, "IceProd2", @L2i ) ;
+insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@L2, "IC86 2022 Offline L2 Data Production", 2021, "L2", 1, 0, "IceProd2", @L2i ) ;
 # update datasets set enabled = 0 where dataset_id = @L2x ;
 select * from datasets where dataset_id = @L2x or dataset_id = @L2 ;
 
 
-insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@C3, "IC86 2021 L3 Data Production for the Cascade WG", 2021, "L3", 1, 3, "IceProd2", @C3i ) ;
+insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@C3, "IC86 2022 L3 Data Production for the Cascade WG", 2021, "L3", 1, 3, "IceProd2", @C3i ) ;
 # update datasets set enabled = 0 where dataset_id = @C3x ;
 select * from datasets where dataset_id = @C3x or dataset_id = @C3 ;
 
@@ -14,7 +14,7 @@ insert into level3_config (dataset_id, path, aggregate) values (@C3, '/data/ana/
 select * from level3_config where dataset_id = @C3x or dataset_id = @C3 ;
 
 
-insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@M3, "IC86 2021 L3 Data Production for the Muon WG", 2021, "L3", 1, 1, "IceProd2", @M3i ) ;
+insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@M3, "IC86 2022 L3 Data Production for the Muon WG", 2021, "L3", 1, 1, "IceProd2", @M3i ) ;
 # update datasets set enabled = 0 where dataset_id = @M3x ;
 select * from datasets where dataset_id = @M3x or dataset_id = @M3 ;
 
@@ -25,7 +25,7 @@ insert into level3_config (dataset_id, path, aggregate) values (@M3, '/data/ana/
 select * from level3_config where dataset_id = @M3x or dataset_id = @M3 ;
 
 
-insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@L4, "IC86 2021 L4 Data Production for the Diffuse/Atmospheric Neutrinos WG", 2021, "L4", 1, 5, "IceProd2", @L4i ) ;
+insert into datasets (dataset_id, description, season, type, enabled, working_group, comment, iceprod_id) values (@L4, "IC86 2022 L4 Data Production for the Diffuse/Atmospheric Neutrinos WG", 2021, "L4", 1, 5, "IceProd2", @L4i ) ;
 # update datasets set enabled = 0 where dataset_id = @L4x ;
 select * from datasets where dataset_id = @L4x or dataset_id = @L4 ;
 
