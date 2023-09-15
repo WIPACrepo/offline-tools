0c6769b671c6283c2db3c4bfbfbca3e9e8466d2f
R Snihur <gh999ic@gmail.com>
2023-09-15 15:57:38 +0000

    Used for pip install

---
+++ OfflineSubmitScripts/seasons/2022/bin/Env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+unset PYTHONPATH
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
+#eval `/cvmfs/icecube.opensciencegrid.org/iceprod/v2.5.3/setup.sh`
+
+# Load config in order to get current L2 icerec path
+eval "$(python3 ${DIR}/bin/ConfigLoader.py)"
+
+echo "I3_BUILD=${Level2[i3_build]}"
+
+"${Level2[i3_build]}/./env-shell.sh" $@
